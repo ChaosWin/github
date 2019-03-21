0e5bbeb0e1980743331e68a8c71dd6a82f1930e3
侠之大者易 <chaos_yi@qq.com>
2019-03-21 16:28:06 +0800

    由 Cloud Studio 更新，https://studio.dev.tencent.com

---
+++ hh.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!你好！我用的Ubuntu\n";
+    return 0;
+}
--- hhh.py
+++ hhh.py
@@ -1,1 +1,10 @@
-print("hh")
+import time
+
+time_start=time.time()
+s = 1
+for i in range(2000):
+    s = s*(i+1)
+print(s)
+
+time_end=time.time()
+print('time cost',time_end-time_start,'s')
